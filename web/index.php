f35210b51bc14199c83b5c981f09572e2b1b98e2
witosmartyn <witosmartyn@mail.ru>
2018-07-31 00:09:40 +0300

    remove source viev

--- web/index.php
+++ web/index.php
@@ -8,10 +8,10 @@
 <body bgcolor="#FFFFFF">
 <H1>PHP/Java Bridge board generation</H1>
 <?php
-    $jspFormLink = "<a href='board/form.jsp'>JSP Form</a> &nbsp;<a href='source.php?source=board.php'>Source php<br> ";
-    $phpFormLink = "<a href='board/form.php'>PHP Form</a> &nbsp;<a href='source.php?source=jspservlet.jsp'>Source JSP</a><br><br>";
-    $infoServlet = "<a href='others/infoServlet'>php info from servlet</a> &nbsp;<a href='source.php?source=others/phpinfo.php'>Source phpinfo.php</a><br>";
-    $phpinfo = "<a href='others/phpinfo.php?info=1'>php info from php</a> &nbsp;<a href='source.php?source=others/phpinfo.php'>Source phpinfo.php</a><br>";
+    $jspFormLink = "<a href='board/form.jsp'>JSP Form</a> <br> ";
+    $phpFormLink = "<a href='board/form.php'>PHP Form</a> <br>";
+    $infoServlet = "<a href='others/infoServlet'>php info from servlet</a> <br>";
+    $phpinfo = "<a href='others/phpinfo.php?info=1'>php info from php</a><br>";
 echo $jspFormLink;
 echo $phpFormLink;
 echo $infoServlet;
